a91dd9492538ee93976294e1353a35f6a2d1530d
Kevin Elgueta <kevinelgueta26@gmail.com>
2022-01-23 17:40:40 -0300

    Agrego NavBar - Desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+
+    <nav className="Navigation">
+        <ul>
+            <li>Home</li>
+            <li>Productos</li>
+            <li>Informacion</li>
+            <li>Contacto</li>
+        </ul>
+    </nav>
+    
+    );
+};
+
+export default NavBar;
